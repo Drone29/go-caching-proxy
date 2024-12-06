dea7d7cb27edb3e9dd5c107a1dbc996505e9e4c6
andrey_dergunov <andrei_dergunov@epam.com>
2024-12-06 20:23:17 +0100

    requests: add backup/restore

--- proxy/request/request.go
+++ proxy/request/request.go
@@ -25,6 +25,15 @@ type auxRequest struct {
 	Body string `json:"body"` //serialize body as string (json will utilize base64)
 }
 
+// store in json file using this format
+type auxRequestWithHost struct {
+	Host     string    `json:"host"`
+	Requests []Request `json:"requests"`
+}
+
+type reqMap = map[string][]Request
+
+// custom json marshaler for request
 func (r *Request) MarshalJSON() ([]byte, error) {
 	aux := &auxRequest{
 		aliasRequest: (*aliasRequest)(r),
@@ -33,6 +42,7 @@ func (r *Request) MarshalJSON() ([]byte, error) {
 	return json.Marshal(aux)
 }
 
+// custom json unmarshaler for request
 func (r *Request) UnmarshalJSON(data []byte) error {
 	aux := &auxRequest{
 		aliasRequest: (*aliasRequest)(r),
@@ -45,24 +55,53 @@ func (r *Request) UnmarshalJSON(data []byte) error {
 	return nil
 }
 
-// backup requests to json file
-func Backup(filename string, requests []Request) error {
+// backup requests for single host to json file
+func BackupOne(filename, host string, requests []Request) error {
+	// read all currently backed up records
+	records, err := RestoreAll(filename)
+	if err != nil {
+		return err
+	}
+	// update map
+	records[host] = requests
+	return BackupMany(filename, records)
+}
+
+// backup multiple requests for multiple hosts
+// requests - a map of host-[]requests pairs
+func BackupMany(filename string, requests reqMap) error {
+	// convert to intermediary types
+	backup := []auxRequestWithHost{}
+	for k, v := range requests {
+		backup = append(backup, auxRequestWithHost{
+			Host:     k,
+			Requests: v,
+		})
+	}
+
 	f, err := os.Create(filename)
 	if err != nil {
 		return err
 	}
 	defer f.Close()
 	encoder := json.NewEncoder(f)
-	return encoder.Encode(requests)
+	return encoder.Encode(backup)
 }
 
-func Restore(filename string) (requests []Request, err error) {
+// restore all requests for all hosts
+// requests - a map of host-[]requests pairs
+func RestoreAll(filename string) (requests reqMap, err error) {
+	requests = make(reqMap)
+	records := []auxRequestWithHost{}
 	f, err := os.Open(filename)
 	if err != nil {
 		return
 	}
 	defer f.Close()
 	decoder := json.NewDecoder(f)
-	err = decoder.Decode(&requests)
+	err = decoder.Decode(&records)
+	for _, v := range records {
+		requests[v.Host] = v.Requests
+	}
 	return requests, err
 }
