327bad15b24e00869255d6c59eb27d53f4ef4cf2
andrey_dergunov <andrei_dergunov@epam.com>
2024-12-06 19:12:04 +0100

    request: json simple operations

--- proxy/request/request.go
+++ proxy/request/request.go
@@ -1,13 +1,68 @@
 package request
 
+import (
+	"encoding/json"
+	"os"
+)
+
 // acts as both request and response
 type Request struct {
-	Body    []byte
-	Headers map[string][]string
+	Body    []byte              `json:"body"`
+	Headers map[string][]string `json:"headers"`
 	// request only
-	Method string
-	Uri    string
+	Method string `json:"-"` // don't need to save this to json
+	Uri    string `json:"-"` // don't need to save this to json
 	// response only
-	RespStatus string
-	RespCode   int
+	RespStatus string `json:"status"`
+	RespCode   int    `json:"code"`
+}
+
+// create an alias to ensure MarshalJSON() is not called recursively
+type aliasRequest Request
+
+type auxRequest struct {
+	*aliasRequest
+	Body string `json:"body"` //serialize body as string (json will utilize base64)
+}
+
+func (r *Request) MarshalJSON() ([]byte, error) {
+	aux := &auxRequest{
+		aliasRequest: (*aliasRequest)(r),
+		Body:         string(r.Body),
+	}
+	return json.Marshal(aux)
+}
+
+func (r *Request) UnmarshalJSON(data []byte) error {
+	aux := &auxRequest{
+		aliasRequest: (*aliasRequest)(r),
+	}
+	if err := json.Unmarshal(data, aux); err != nil {
+		return err
+	}
+	// convert body to []byte
+	r.Body = []byte(aux.Body)
+	return nil
+}
+
+// backup requests to json file
+func Backup(filename string, requests []Request) error {
+	f, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	encoder := json.NewEncoder(f)
+	return encoder.Encode(requests)
+}
+
+func Restore(filename string) (requests []Request, err error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+	decoder := json.NewDecoder(f)
+	err = decoder.Decode(&requests)
+	return requests, err
 }
